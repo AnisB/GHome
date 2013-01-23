13b3b6d71446b7fd77f7acc5257846c1b31ab0d5
Anis <Benyoub.anis@gmail.com>
2013-01-23 11:33:35 +0100

    Adding

---
+++ ServerGHome/src/dao/ClientDao.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import javax.persistence.Query;
+import modele.Click;
+import modele.Client;
+import util.JpaUtil;
+
+/**
+ *
+ * @author anisbenyoub
+ */
+public class ClientDao {
+     public ClientDao(){
+        
+        }
+         public void create(String id,  String mdp) throws Exception  {
+
+        Client newC = new Client(id,mdp);
+        JpaUtil.openEntityManager();
+        JpaUtil.getEntityManagerTransaction().begin();
+        JpaUtil.getEntityManager().persist(newC);
+        JpaUtil.getEntityManagerTransaction().commit();
+        JpaUtil.closeEntityManager();
+    }
+       public boolean testIfRegistred(String id, String mdp)
+       {   
+            Query query = (Query) JpaUtil.getEntityManager().createQuery("SELECT c from Client c WHERE c.login=:id");
+            Client c = (Client)query.setParameter("id", id).getSingleResult();
+            if(c!=null)
+                return true;
+            else
+                return false;
+       }
+    
+
+}
