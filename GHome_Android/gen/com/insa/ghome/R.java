d4def09807bf47f07e3b13117e3d5e1617beefa9
Jeremy Gaillard <jeremy.gaillard@insa-lyon.fr>
2013-01-30 08:30:26 +0100

    created main window on Android

--- GHome_Android/gen/com/insa/ghome/R.java
+++ GHome_Android/gen/com/insa/ghome/R.java
@@ -14,11 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f070009;
         public static final int buttonConnect=0x7f070006;
+        public static final int editText1=0x7f070008;
         public static final int editTextIP=0x7f070005;
         public static final int editTextLogin=0x7f070001;
         public static final int editTextPW=0x7f070003;
-        public static final int menu_settings=0x7f070008;
+        public static final int menu_settings=0x7f07000a;
         public static final int textView1=0x7f070007;
         public static final int textViewIP=0x7f070004;
         public static final int textViewLogin=0x7f070000;
@@ -26,6 +28,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int main_window=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
@@ -34,10 +37,13 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int buttonConnection=0x7f040003;
         public static final int hello_world=0x7f040001;
-        public static final int ip=0x7f040006;
-        public static final int login=0x7f040004;
+        public static final int ip=0x7f040009;
+        public static final int login=0x7f040007;
         public static final int menu_settings=0x7f040002;
-        public static final int password=0x7f040005;
+        public static final int password=0x7f040008;
+        public static final int string_ip=0x7f040006;
+        public static final int string_login=0x7f040004;
+        public static final int string_password=0x7f040005;
     }
     public static final class style {
         /** 
--- GHome_Android/src/com/insa/ghome/MainActivity.java
+++ GHome_Android/src/com/insa/ghome/MainActivity.java
@@ -1,8 +1,8 @@
 package com.insa.ghome;
 
-
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -15,60 +15,68 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	protected TextView coucou = null;
-//	protected EditText log = null;
+	// protected EditText log = null;
 	protected EditText ip = null;
 	protected EditText login = null;
 	protected EditText password = null;
 	protected BlockingQueue<String> inQueue;
 	protected BlockingQueue<String> outQueue;
 
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        coucou = (TextView)findViewById(R.id.textView1);
-        ip = (EditText)findViewById(R.id.editTextIP);
-        login = (EditText)findViewById(R.id.editTextLogin);
-        password = (EditText)findViewById(R.id.editTextPW);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        coucou.setText(R.string.hello_world);
-        final Button bConnect = (Button)findViewById(R.id.buttonConnect);
-        
-        inQueue = new LinkedBlockingQueue<String>();
-        outQueue = new LinkedBlockingQueue<String>();
-        bConnect.setOnClickListener(new View.OnClickListener() {
-        	
-			
+		coucou = (TextView) findViewById(R.id.textView1);
+		ip = (EditText) findViewById(R.id.editTextIP);
+		login = (EditText) findViewById(R.id.editTextLogin);
+		password = (EditText) findViewById(R.id.editTextPW);
+
+		coucou.setText(R.string.hello_world);
+		final Button bConnect = (Button) findViewById(R.id.buttonConnect);
+
+		inQueue = new LinkedBlockingQueue<String>();
+		outQueue = new LinkedBlockingQueue<String>();
+		bConnect.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View arg0) {
 				String serverAddress = ip.getText().toString();
 				coucou.setText(serverAddress);
-				
-	            Thread emission = new Thread(new EmissionThread(serverAddress, outQueue, inQueue));
-				
-	            emission.start();
-	            
-	            String identificationMessage = "C " + login.getText().toString()
-	            		+ " " + password.getText().toString();
-	            try {
+
+				Thread emission = new Thread(new EmissionThread(serverAddress,
+						outQueue, inQueue));
+
+				emission.start();
+
+				String identificationMessage = "C "
+						+ login.getText().toString() + " "
+						+ password.getText().toString();
+				try {
 					outQueue.put(identificationMessage);
+					String answer = inQueue.poll(5000, TimeUnit.MILLISECONDS);
+					// TODO change hard-written values
+					if (answer == null) {
+						// TODO : notify timeout to user
+					} else {
+						if (answer.compareTo("C1") == 0) {
+							setContentView(R.layout.main_window);
+						}
+					}
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		});
-//        setContentView(R.layout.activity_main);
-    }
-    
+		// setContentView(R.layout.activity_main);
+	}
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.activity_main, menu);
-//        return true;
-//    }
-    
+	// @Override
+	// public boolean onCreateOptionsMenu(Menu menu) {
+	// // Inflate the menu; this adds items to the action bar if it is present.
+	// getMenuInflater().inflate(R.menu.activity_main, menu);
+	// return true;
+	// }
+
 }
